497862fa5c1a2817644fac05b2fd20b0666dc054
Brian P. Holt <bholt@planetholt.com>
2021-09-14 01:24:55 -0500

    use error channel in EitherT and OptionT when it's available

--- core/shared/src/main/scala/org/scalacheck/cats/instances/Arbitraries.scala
+++ core/shared/src/main/scala/org/scalacheck/cats/instances/Arbitraries.scala
@@ -6,6 +6,7 @@ import cats.syntax.all._
 import org.scalacheck._
 import org.scalacheck.Arbitrary.arbitrary
 import org.scalacheck.cats.implicits._
+import org.scalacheck.cats.instances.PartiallyAppliedFromOneAnd.fromOneAnd
 
 import scala.collection.immutable._
 
@@ -15,13 +16,17 @@ private[instances] class PartiallyAppliedFromOneAnd[G[_]](val dummy: Unit = ()) 
     arbitrary[OneAnd[F, A]].map { case OneAnd(head, tail) => f(head, tail) }
 }
 
-trait Arbitraries {
+object PartiallyAppliedFromOneAnd {
+  private[instances] def fromOneAnd[G[_]] = new PartiallyAppliedFromOneAnd[G]
+}
+
+trait Arbitraries extends Arbitraries1
+
+sealed private[instances] trait Arbitraries1 extends Arbitraries0 {
   def genOneAnd[F[_], A: Arbitrary](implicit F: Arbitrary[F[A]]): Gen[OneAnd[F, A]] =
     (arbitrary[A], arbitrary[F[A]]).mapN(OneAnd(_, _))
 
   implicit def arbOneAnd[F[_], A: Arbitrary](implicit F: Arbitrary[F[A]]): Arbitrary[OneAnd[F, A]] = Arbitrary(genOneAnd[F, A])
-
-  private def fromOneAnd[G[_]] = new PartiallyAppliedFromOneAnd[G]
 
   def genNonEmptyList[A: Arbitrary]: Gen[NonEmptyList[A]] =
     fromOneAnd[NonEmptyList](NonEmptyList[A](_, _))
@@ -63,6 +68,22 @@ trait Arbitraries {
 
   implicit def arbNonEmptyMap[K : Arbitrary : Order, V: Arbitrary]: Arbitrary[NonEmptyMap[K, V]] = Arbitrary(genNonEmptyMap[K, V])
 
+  def genEitherTWithErrorChannel[F[_], A : Arbitrary, B : Arbitrary, C : Arbitrary](implicit F: ApplicativeError[F, C]): Gen[EitherT[F, A, B]] =
+    arbitrary[Either[C, Either[A, B]]].map {
+      case Left(c) => EitherT(c.raiseError[F, Either[A, B]])
+      case Right(ab) => EitherT.fromEither[F](ab)
+    }
+
+  implicit def arbEitherTWithErrorChannel[F[_], A : Arbitrary, B : Arbitrary, C : Arbitrary](implicit F: ApplicativeError[F, C]): Arbitrary[EitherT[F, A, B]] = Arbitrary(genEitherTWithErrorChannel[F, A, B, C])
+
+  def genOptionTWithErrorChannel[F[_], A : Arbitrary, C : Arbitrary](implicit F: ApplicativeError[F, C]): Gen[OptionT[F, A]] =
+    arbitrary[Either[C, Option[A]]].map {
+      case Left(c) => OptionT(c.raiseError[F, Option[A]])
+      case Right(a) => OptionT.fromOption[F](a)
+    }
+}
+
+sealed private[instances] trait Arbitraries0 {
   def genEitherT[F[_] : Applicative, A, B](implicit arb: Arbitrary[Either[A, B]]): Gen[EitherT[F, A, B]] =
     arbitrary[Either[A, B]].map(EitherT.fromEither[F](_))
 
